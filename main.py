52cce0586ef5591e8a56fb395fd2d6115ebe4d55
thanwapong2531-stack <thanwapong2531@gmail.com>
2026-03-05 11:11:08 +0700

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from flask import Flask, request
-import os
 
 app = Flask(__name__)
 
-# หน้าแรก (เอาไว้เช็คว่า server ทำงาน)
 @app.route("/")
 def home():
-    return "AI LINE BOT RUNNING"
+    return "Bot is running"
 
-# webhook จาก LINE
 @app.route("/callback", methods=["POST"])
 def callback():
-    body = request.get_json()
+    body = request.json
     print(body)
-    return "OK", 200
-
+    return "OK"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
