dbd6b5b35c9c27e78ee8fcd52fef396c194eaea1
thanwapong2531-stack <thanwapong2531@gmail.com>
2026-03-05 10:21:29 +0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,59 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+import openai
+import os
 
+app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+openai.api_key = OPENAI_API_KEY
+
+
+@app.route("/", methods=['GET'])
+def home():
+    return "AI LINE BOT RUNNING"
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    user_text = event.message.text
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": user_text}
+        ]
+    )
+
+    ai_reply = response.choices[0].message.content
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=ai_reply)
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
