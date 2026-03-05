aff818109c5e3505730a20c7d1e78cb75c3b65b7
thanwapong2531-stack <thanwapong2531@gmail.com>
2026-03-05 11:14:24 +0700

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
+
+CHANNEL_ACCESS_TOKEN = "ใส่ChannelAccessTokenตรงนี้"
 
 @app.route("/")
 def home():
@@ -9,8 +13,37 @@ def home():
 @app.route("/callback", methods=["POST"])
 def callback():
     body = request.json
-    print(body)
+    events = body.get("events", [])
+
+    for event in events:
+        if event["type"] == "message":
+            replyToken = event["replyToken"]
+            text = event["message"]["text"]
+
+            reply(replyToken, "คุณพิมพ์ว่า: " + text)
+
     return "OK"
+
+def reply(token, message):
+
+    url = "https://api.line.me/v2/bot/message/reply"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + CHANNEL_ACCESS_TOKEN
+    }
+
+    data = {
+        "replyToken": token,
+        "messages":[
+            {
+                "type":"text",
+                "text":message
+            }
+        ]
+    }
+
+    requests.post(url, headers=headers, json=data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
